aaeba255099a0e68436099f5567d669cb90a17f5
AbbosbekNorinboyev <norinboyevabboskhan002@gmail.com>
2026-05-13 19:28:24 +0500

    rabbitmq with mqtt protocol

---
+++ src/main/java/uz/brb/rabbitmqwithmqttprotokol/RabbitMqWithMqttProtocolApplication.java
@@ -0,0 +1,13 @@
+package uz.brb.rabbitmqwithmqttprotokol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqWithMqttProtocolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqWithMqttProtocolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/brb/rabbitmqwithmqttprotokol/RabbitMqWithMqttProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.brb.rabbitmqwithmqttprotokol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqWithMqttProtocolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
